4f1dc0c166024ff2a0c974bbb5259ebf4be5bf3a
dimeritium-foil <farrisessam@protonmail.com>
2020-03-20 02:20:27 +0200

    added parabola's blacklist and check if any common packages exist in the installeg packages

--- stallman.sh
+++ stallman.sh
@@ -16,10 +16,28 @@ fi
 
 case $distro in
     arch)
-        # todo: curl the blacklist from parabola's site
-        #       then diff it with the list of packages
-        #       and prompt to remove
-        echo $distro
+        temp_blacklist="/tmp/temp_blacklist.txt"
+        pkg_del="/tmp/pkg_del.txt"
+        installed_pkgs="/tmp/installed_pkgs.txt"
+        proprietary_pkgs="/tmp/proprietary_pkgs.txt"
+
+        pacman -Qqn > $installed_pkgs
+
+        curl https://git.parabola.nu/blacklist.git/plain/blacklist.txt > $temp_blacklist
+
+        sed -i '/branding/ d' $temp_blacklist
+        egrep -o '^[a-z,A-Z,0-9,-]+' $temp_blacklist > $pkg_del
+
+        comm --nocheck-order -1 -2 $pkg_del $installed_pkgs > $proprietary_pkgs
+
+        echo -e "The following packages are absolutely proprietary and must be purged immediately: \n"
+        cat $proprietary_pkgs 
+
+        # todo: prompt the user for confirmation then delete the packages
+        #       replace the deleted packages with free alternatives if available
+        #
+        # double check the blacklist bec some essential packages are reported as
+        # proprietary such as pacman and base`;w
         ;;
     *)
         echo "Your distro is yet to be supported"
